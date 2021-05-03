ca7d070332d8bca09a4e18095ee2ede6abc692b3
Kerriea-star <ojared511@gmail.com>
2021-05-03 22:26:05 +0300

    Date Formatting

---
+++ Date Formatting.py
@@ -0,0 +1,22 @@
+#  Time between two date-times
+from datetime import datetime
+a = datetime(2016, 10, 6, 0, 0, 0)
+b = datetime(2016, 10, 1, 23, 59, 59)
+
+a-b
+
+(a-b).days
+(a-b).toatal_seconds()
+
+# Outputting total object to string
+# Uses C standard format
+from datetime import datetime
+datetime_for_string = datetime(2016, 21, 1, 0, 0)
+datetime_string_format = '%b %d %Y, #H:%M:%S'
+datetime.strftime(datetime_for_string, datetime_string_format)
+
+# Parsing string to datetime object
+from datetime import datetime
+datetime_string = 'Oct 1 2016, 00:00:00'
+datetime_string_format = '%b %d %Y, %H:%M:%S'
+datetime.strptime(datetime_string, datetime_string_format)
