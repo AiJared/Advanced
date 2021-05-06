2aef431ed628018c1752600685d2db95aed3b4e4
Kerriea-star <ojared511@gmail.com>
2021-05-06 17:25:24 +0300

    Operator precedence

---
+++ Operator Precedence.py
@@ -0,0 +1,10 @@
+a, b, c, d = 2, 3, 5, 7
+a ** (b + c)
+a * b ** c
+a + b * c / d
+
+# Mathematical rules hold but not always
+300 / 300 * 200
+300 * 200 / 300
+1e300 / 1e300 * 1e200
+1e300 * 1e200 / 1e300
