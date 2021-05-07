bdee925fa98e42f2d17a2277fa0036a42a9036c8
kerriea-star <ojared511@gmail.com>
2021-05-07 17:20:19 +0300

    Variables

--- Variable scope and binding.py
+++ Variable scope and binding.py
@@ -8,3 +8,30 @@ def counter():
 c = counter ()
 print(c)
 print(c())
+
+# Global Variables
+x = 'Hi'
+
+def read_x():
+    print(x)
+print(read_x())
+
+def read_y():
+    y = 'Hey'
+    print(y)
+print(read_y())
+
+x = 'Hi'
+def change_local_x():
+    x = 'Bye'
+    print(x)
+change_local_x()
+print(x)
+
+x = 'Hi'
+def change_global_x():
+    global x
+    x = 'Bye'
+    print(x)
+change_global_x() # prints Bye
+print(x)
