73f39cf790a50b32aded52b339b3b9cfdcf1786c
Kerriea-star <ojared511@gmail.com>
2021-05-13 09:37:35 +0300

    Loops

--- Loops.py
+++ Loops.py
@@ -52,3 +52,29 @@ for index, item in enumerate(['one', 'two', 'three', 'four']):
 #  Iterate over lists value manipulation using map and lambda
 x = map(lambda e : e.upper(), ['one', 'two', 'three', 'four'])
 print(x)
+#  Loops with an else clause
+for i in range(3):
+    print(i)
+else:
+    print('done')
+i = 0
+while i < 3:
+    print(i)
+    i += 1
+else:
+    print('done')
+#  else does not execute if the loop terminates through s break or exeption
+for i in range(2):
+    print(i)
+    if i == 1:
+        break
+    else:
+        print('done')
+# The main use case for the for...else construct is a concise implementation of search as for instance:
+a = [1, 2, 3, 4]
+for i in a:
+    if type(i) is not int:
+        print(i)
+        break
+    else:
+        print('no exception')
