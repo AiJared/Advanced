a7fd1a1f55f30048d8fa0c459b5d847e37ff272a
Kerriea-star <ojared511@gmail.com>
2021-05-15 09:48:41 +0300

    Arrays

--- Arrays.py
+++ Arrays.py
@@ -1,5 +1,11 @@
 #  Access Individual elements through indexes
-my_array = ('i', [1, 2, 3, 4, 5])
+from array import *
+my_array = array('i', [1, 2, 3, 4, 5])
 print(my_array[1])
 print(my_array[2])
 print(my_array[0])
+for i in my_array:
+    print(i)
+#  Append any value to the array using append() method
+my_array.append(6)
+print(my_array)
