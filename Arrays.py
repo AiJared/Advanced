8e006d15dae821b7a713d0ee4d531f3ff30fabb1
Kerriea-star <ojared511@gmail.com>
2021-05-15 10:07:42 +0300

    Arrays

--- Arrays.py
+++ Arrays.py
@@ -9,3 +9,22 @@ for i in my_array:
 #  Append any value to the array using append() method
 my_array.append(6)
 print(my_array)
+my_array.insert(0,0)
+print(my_array)
+#  Extend Array using extend() method
+my_extnd_array = array('i', [7, 8, 8, 10])
+my_array.extend(my_extnd_array)
+print(my_array)
+#  Add items from list into array using fromlist() method
+c = [11, 12, 13]
+my_array.fromlist(c)
+print(my_array)
+#  Remove any array element using remove() method
+my_array.remove(4)
+print(my_array)
+#  Remove last element using pop() method
+my_array.pop()
+print(my_array)
+#  Fetch any element through its index using index() method
+print(my_array.index(5))
+print(my_array.index(3))
